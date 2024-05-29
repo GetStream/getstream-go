e56abc402eac133c7507ef0d2e0c58bf17d29971
sachaarbonel <sacha.arbonel@hotmail.fr>
2024-05-29 12:35:21 +0530

    version

---
+++ version.go
@@ -0,0 +1,27 @@
+package stream_go
+
+import (
+	"fmt"
+)
+
+const (
+	versionMajor = 0
+	versionMinor = 0
+	versionPatch = 1
+)
+
+// Version returns the version of the library.
+func Version() string {
+	return "v" + fmtVersion()
+}
+
+func versionHeader() string {
+	return "stream-go-client-" + fmtVersion()
+}
+
+func fmtVersion() string {
+	return fmt.Sprintf("%d.%d.%d",
+		versionMajor,
+		versionMinor,
+		versionPatch)
+}
