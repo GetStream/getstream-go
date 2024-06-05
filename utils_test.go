8bf61c261768fdc8255176f9cb233a80f45f26ca
sachaarbonel <sacha.arbonel@hotmail.fr>
2024-06-05 16:02:09 +0530

    fix randomString util test

--- utils_test.go
+++ utils_test.go
@@ -1,15 +1,20 @@
 package getstream
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 func PtrTo[T any](v T) *T {
 	return &v
 }
 
 func randomString(n int) string {
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	bytes := make([]byte, n)
 	for i := 0; i < n; i++ {
-		bytes[i] = byte(65 + rand.Intn(25)) // A=65 and Z = 65+25
+		bytes[i] = byte(65 + r.Intn(26)) // should be 26 to include 'Z'
 	}
 	return string(bytes)
 }
