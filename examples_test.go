67512715584b5491d71c52fde1531e4db2fc4947
Aditya Agarwal <itsmeadityaagarwal@gmail.com>
2025-08-13 14:27:32 +0200

    bug: update go.mod

--- examples_test.go
+++ examples_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/GetStream/getstream-go"
+	"github.com/GetStream/getstream-go/v3"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
